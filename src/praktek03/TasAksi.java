c6249e894521da5e59f6a39dec3d11f2b16c452f
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2018-04-08 12:30:24 +0800

    Instance,buat atribut dan panggil method

--- src/praktek03/TasAksi.java
+++ src/praktek03/TasAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class TasAksi {
     public static void main(String[] args) {
+        Tas t = new Tas();
+        t.merk="Jansport";
+        t.warna="Hitam";
+        t.harga=500000;
         
+        t.cetakInfo();
     }
 }

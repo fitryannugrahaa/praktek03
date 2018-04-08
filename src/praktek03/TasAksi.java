417fff935174c314bddd6cbe5f43652ded296dce
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2018-04-08 12:47:07 +0800

    menggunakan method set

--- src/praktek03/TasAksi.java
+++ src/praktek03/TasAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class TasAksi {
     public static void main(String[] args) {
         Tas t = new Tas();
-        t.merk="Jansport";
-        t.warna="Hitam";
-        t.harga=500000;
+
+        t.setMerk("Jansport");
+        t.setWarna("Hitam");
+        t.setHarga(500000);
         
         t.cetakInfo();
     }
